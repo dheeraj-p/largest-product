30b6b8d2552c8c00282efdbe4b6508a170ed028d
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-10-14 14:59:46 +0530

    Initial Commit - Program for finding largest product in series.
    This is a script for finding largest product of 'n' adjacent digits
    in a number.
    This script has passed tests for the product of 2,3,5 and 13 adjacent
    digits.

---
+++ largest_product.js
@@ -0,0 +1,18 @@
+const calculateLargestProduct = function (number, numberOfAdjacentDigits){
+  let digits = number.split("");
+  let largestProduct = 0;
+  let threshold  = digits.length - numberOfAdjacentDigits + 1;
+  for(let digitCount = 0; digitCount < threshold; digitCount++){
+    let currentProduct = 1;
+    for(let index = digitCount; index < digitCount + numberOfAdjacentDigits; index++){
+      currentProduct = currentProduct * digits[index];
+    }
+    if(currentProduct > largestProduct){
+      largestProduct = currentProduct;
+    }
+  }
+  return largestProduct;
+}
+
+exports.calculateLargestProduct = calculateLargestProduct;
+
